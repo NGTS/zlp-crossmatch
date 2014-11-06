7398a76710ad90b13fec40dfce95eb470bc7bb06
Simon Walker <s.r.walker101@googlemail.com>
2014-11-06 14:32:19 +0000

    Initial commit

---
+++ zlp_crossmatch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+import argparse
+import subprocess as sp
+
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    main(parser.parse_args())
